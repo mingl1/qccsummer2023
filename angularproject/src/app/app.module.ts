d08dd3250557b4d283866124a6f0b92df9166eb5
mingl1 <linm1@bxscience.edu>
2023-09-02 17:26:54 -0400

    angular proj

--- angularproject/src/app/app.module.ts
+++ angularproject/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ProductComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
