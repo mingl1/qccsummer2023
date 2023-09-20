5e2b2a99fdbf4e519636d630322a66c4527bc118
mingl1 <linm1@bxscience.edu>
2023-09-20 11:33:07 -0400

    fix proj2

--- proj2/dolist/views.py
+++ proj2/dolist/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
+from .models import Todolist
 # Create your views here.
 def index(request):
-    return render(request, 'dolist/index.html')
+    todo_items = Todolist.objects.order_by('id')
+    context = {'todo_items':todo_items}
+    if request.method=='POST':
+        # print(request.POST)
+        if request.POST.get('del')=='del_completed':
+            Todolist.objects.all().filter(completed=True).delete()
+        else:
+            Todolist.objects.all().delete()
+    
+    return render(request, 'dolist/index.html',context)
